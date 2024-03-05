2fdc07e6355d21ce390da2f9ca2ebb96aff626ab
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-03-05 17:37:51 +0530

    Created the signup endpoint and changed login to signin

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const { MongoClient } = require('mongodb')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,27 +8,26 @@ router.get('/', function(req, res, next) {
 });
 
 //Middleware to check if the username or password is empty
-router.use(['/login','/signup'],function(req, res, next){
-  if(req.body.username==='' || req.body.password===''){
-    res.send({message: 'Username or password cannot be empty'});
+router.use(['/signin','/signup'],function(req, res, next){
+  if(Object.keys(req.body).length === 0){
+    res.send({success: false, message: 'Request body cannot be empty'});
+  }
+  else if(req.body.username==='' || req.body.password===''){
+    res.send({success: false, message: 'Username or password cannot be empty'});
   } else {
     next();
   }
 })
 
-/* POST login */
-router.post('/login', (req, res, next)=>{
-  const { MongoClient } = require('mongodb');
-  // or as an es module:
-  // import { MongoClient } from 'mongodb'
-
+/* POST signin */
+router.post('/signin', (req, res, next)=>{
   // Connection URL
   const url = 'mongodb://localhost:27017';
   const client = new MongoClient(url);
 
   // Database Name
   const dbName = 'dbcrafter';
-  let authenticated = false;
+  let result = {success: false, message: 'Not Signed In'};
   async function main() {
     // Use connect method to connect to the server
     await client.connect();
@@ -38,18 +38,18 @@ router.post('/login', (req, res, next)=>{
     // the following code examples can be pasted here...
     const findResult = await collection.findOne({username: req.body.username, password: req.body.password});
     if(findResult){
-      authenticated = true
+      result.success = true;
+      result.message = "Sign in successfull";
+    } else {
+      result.success = false;
+      result.message = "Invalid username or password";
     }
     return 'done.';
   }
 
   main()
     .then(()=>{
-      if(authenticated){
-        res.send({authenticated: true, message: "Login successful"})
-      } else {
-        res.send({authenticated: false, message: "Account not found"})
-      }
+      res.send(result)
     })
     .catch(console.Error)
     .finally(() => client.close());
@@ -57,32 +57,34 @@ router.post('/login', (req, res, next)=>{
 
 /*POST signup*/
 router.post('/signup', (req, res, next) => {
-  const { MongoClient } = require('mongodb');
-  // or as an es module:
-  // import { MongoClient } from 'mongodb'
-
   // Connection URL
   const url = 'mongodb://localhost:27017';
   const client = new MongoClient(url);
-
+  
   // Database Name
   const dbName = 'dbcrafter';
-
+  let created = {success: false, message: 'Cannot Create Account'}
   async function main() {
     // Use connect method to connect to the server
     await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
     const collection = db.collection('users');
-  
+    
     // the following code examples can be pasted here...
-    const insertResult = await collection.insertOne({username: req.body.username, password: req.body.password});
-    console.log('Inserted document =>', insertResult);
-    return 'done.';
+    let already = await collection.findOne({username: req.body.username})
+    if(!already){
+      created.success = true;
+      created.message = 'Successfully signed up';
+      await collection.insertOne({username: req.body.username, password: req.body.password}); 
+    } else {
+      created.success = false;
+      created.message = 'Username is not available';
+    }
   }
 
   main()
-    .then(res.send('Signup Successful'))
+    .then(()=>{ res.send(created) }) //Remember, this accepts a method, not a function call, passing a function call leads to huge problems
     .catch(console.error)
     .finally(() => client.close());
 })
