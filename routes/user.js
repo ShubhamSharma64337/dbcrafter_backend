8e607b4635b7ceb62ad779abb6946cf6b5396211
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-04-03 11:48:26 +0530

    Modified the creatediagram route to also store the isPublic attribute of the diagram, added gettemplates and gettemplate routes to allow any user to access the public diagrams created by others

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { MongoClient } = require('mongodb')
+var ObjectId = require('mongodb').ObjectId; 
 
 let url = 'mongodb://localhost:27017/dbcrafter'
 
@@ -91,7 +92,11 @@ router.use('/creatediagram', function(req, res, next){
   if(/^\s*$/.test(req.body.name) || req.body.name === null){
     res.send({success: false, message: "Diagram name cannot be empty!"});
     return;
-  } else {
+  } else if(req.body.isPublic === null) {
+    res.send({success: false, message: "Is public attribute must be defined!"});
+    return;
+  }
+  else {
     next();
   }
 })
@@ -116,7 +121,7 @@ router.post('/creatediagram', function(req, res, next){
     if(!already){
       created.success = true;
       created.message = 'Diagram successfully created and saved!';
-      await collection.insertOne({uid: req.session.user.uid, name: req.body.name, tbls: req.body.tbls}); 
+      await collection.insertOne({uid: req.session.user.uid, name: req.body.name, isPublic: req.body.isPublic, tbls: req.body.tbls}); 
     } else {
       created.success = false;
       created.message = 'Diagram with same name already exists!';
@@ -237,4 +242,69 @@ router.post('/renamediagram', function(req, res, next){
     .catch(console.error)
     .finally(() => client.close());
 })
+
+//this returns a list of names and _id for all the diagrams which have isPublic attribute set to true
+router.get('/gettemplates',function (req, res, next){
+  // Connection URL
+  const client = new MongoClient(url);
+   
+  // Database Name
+  const dbName = 'dbcrafter';
+  let created = {success: false, message: 'No templates available!'}
+  async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('diagrams');
+    
+    // Here we find diagrams which are public, and are not created by the current user
+    let already = await collection.find({isPublic: true, uid: { $not: { $eq: req.session.user.uid}}}, {projection: {tbls: 0, uid: 0, isPublic: 0}}).toArray();
+    if(already.length>0){
+      created.success = true;
+      created.message = already
+    } else {
+      created.success = false;
+      created.message = 'No public templates available!';
+    }
+  }
+
+  main()
+    .then(()=>{ res.send(created) }) //Remember, this accepts a method, not a function call, passing a function call leads to huge problems
+    .catch(console.error)
+    .finally(() => client.close());
+}
+)
+
+//get the template specified by the name send in the request
+router.post('/gettemplate', function(req,res, next){
+  // Connection URL
+  const client = new MongoClient(url);
+  
+  // Database Name
+  const dbName = 'dbcrafter';
+  let created = {success: false, message: 'Cannot find template with specified id'}
+  async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const collection = db.collection('diagrams');
+    let objIdObject = new ObjectId(req.body._id); //we must create an ObjectId object, we cannot directly compare _id with a string value
+    let already = await collection.findOne({_id: objIdObject, isPublic: true},{projection: {_id: 0, uid: 0, isPublic: 0}})
+    if(!already){
+      created.success = false;
+      created.message = 'Cannot find template with specified id!';
+    } else {
+      created.success = true;
+      created.message = already;
+    }
+  }
+
+  main()
+    .then(()=>{ res.send(created)}) //Remember, this accepts a method, not a function call, passing a function call leads to huge problems
+    .catch(console.error)
+    .finally(() => client.close());
+})
+
 module.exports = router;
